6f1fef4da53cfa0743e8fbeecbbe318aabfd8507
JakubTesar <jakub.tesar@educanet.cz>
2021-05-04 00:42:56 +0200

    Start

---
+++ src/data/BankAccount.java
@@ -0,0 +1,8 @@
+package data;
+
+public class BankAccount {
+
+    private int money;
+    private int accName;
+
+}
---
+++ src/logic/Bank.java
@@ -0,0 +1,6 @@
+package logic;
+
+public class Bank {
+
+    private String name;
+}
